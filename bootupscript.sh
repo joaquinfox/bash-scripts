da29cdba240118d35b1a769bc6ec2fee6ad920c8
joaquin <joaquin.fox@gmail.com>
2021-09-28 21:37:21 -0700

    first script

---
+++ bootupscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# configure IDE for USB env
+
+# get chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install google-chrome-stable
+
+# get curl
+sudo apt install curl
+
+# get node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node
+nvm list
+
+# get vscode
+sudo apt install software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+
