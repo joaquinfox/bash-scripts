2a0c35291270904235a4b426e929da6b39d6b452
Jimmy <joaquin.fox@gmail.com>
2021-09-30 17:13:28 -0700

    print random word demo

--- demo7_random_word.sh
+++ demo7_random_word.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # This script prints a random word from /usr/share/dict/words
 
-# Step1: capture length of input file
-arr=$(cat ../../../../usr/share/dict/words) 
+# Step1: capture length of input file, used in generating the random value
 len=$(wc -w ../../../../usr/share/dict/words)
-echo $arr
-echo Length: $len
+# echo Length: $len
 
-# Step2: generate a random number with the length range.
+# Step2: capture the list in a variable
+arr=$(cat ../../../../usr/share/dict/words)
+# echo List: $arr
+
+# Step3: generate a random number with the length range.
 rand=$(( $RANDOM % 1021402))
-echo Random Number: $rand
+# echo Random Number: $rand
 
-echo ${arr[$rand]}
+# Step4: print a random number from the list
+echo  $arr | cut -d " " -f $rand
